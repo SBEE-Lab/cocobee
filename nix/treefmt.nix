b3fc8c11a8324bf8b3017ce63f468b01e9d1d7bd
mulatta <67085791+mulatta@users.noreply.github.com>
2026-09-22 21:33:08 +0900

    treefmt: add stuffs

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,10 +1,28 @@
 {
   projectRootFile = "flake.nix";
-  programs = {
-    deadnix.enable = true;
-    nixfmt.enable = true;
-    statix.enable = true;
-    ruff-check.enable = true;
-    ruff-format.enable = true;
-  };
+
+  programs.deadnix.enable = true;
+  programs.nixfmt.enable = true;
+
+  programs.mdformat.enable = true;
+
+  programs.shellcheck.enable = true;
+  programs.shfmt.enable = true;
+
+  programs.taplo.enable = true;
+  programs.yamlfmt.enable = true;
+
+  programs.ruff-check.enable = true;
+  programs.ruff-format.enable = true;
+
+  settings.formatter.deadnix.priority = 1;
+  settings.formatter.nixfmt.priority = 3;
+
+  settings.formatter.shellcheck.priority = 1;
+  settings.formatter.shfmt.priority = 2;
+
+  settings.formatter.ruff-check.priority = 1;
+  settings.formatter.ruff-format.priority = 2;
+
+  settings.global.excludes = [ "**/.direnv/**" ];
 }
